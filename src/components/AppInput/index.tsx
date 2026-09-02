b84b98e4a365bf17a848f4e300d469271a6b59a9
Tassio Neves <tassions@hotmail.com>
2026-09-02 15:57:30 -0300

    feat: add clsx for conditional class names in AppInput component

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -8,6 +8,8 @@ import {
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/shared/colors";
+import { useRef, useState } from "react";
+import { clsx } from "clsx";
 
 interface AppInputParams<T extends FieldValues> extends TextInputProps {
   control: Control<T>;
@@ -23,21 +25,52 @@ export const AppInput = <T extends FieldValues>({
   leftIconName,
   ...rest
 }: AppInputParams<T>) => {
+  const inputRef = useRef<TextInput>(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const checkFocus = () => {
+    if (inputRef.current) {
+      setIsFocused(inputRef.current.isFocused());
+    }
+  };
   return (
     <Controller
       control={control}
       name={name}
       render={({ field: { onChange, value } }) => {
         return (
-          <View className="w-full">
-            {lable && <Text className="text-white">{lable}</Text>}
+          <View className="w-full mt-4">
+            {lable && (
+              <Text
+                className={clsx(
+                  "mb-2 mt-3 text-base ",
+                  isFocused ? "text-accent-brand" : "text-gray-600",
+                )}
+              >
+                {lable}
+              </Text>
+            )}
 
             <TouchableOpacity className="flex-row items-center justify-between border-b border-gray-600 px-3 py-2 h-16">
+              {leftIconName && (
+                <MaterialIcons
+                  name={leftIconName}
+                  size={24}
+                  color={
+                    isFocused ? colors["accent-brand"] : colors.gray["600"]
+                  }
+                  className="mr-2"
+                />
+              )}
               <TextInput
                 value={value}
                 onChangeText={onChange}
                 {...rest}
-                placeholderTextColor={colors.gray[700]}
+                placeholderTextColor={colors.gray["700"]}
+                className="flex-1 text-base text-gray-500"
+                onFocus={checkFocus}
+                onEndEditing={checkFocus}
+                ref={inputRef}
               />
             </TouchableOpacity>
           </View>
