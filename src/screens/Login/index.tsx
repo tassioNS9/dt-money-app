43c5f4ef7c395bc242fb0fb18419901ae53c669b
Tassio Neves <tassions@hotmail.com>
2026-09-02 15:07:27 -0300

    fix: update import path for PublicStackParamsList in Login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,7 +1,7 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { PublicStackParamsList } from "@/routes";
+import { PublicStackParamsList } from "@/routes/PublicRoutes";
 
 export const Login = () => {
   const navigation =
