f84b3abdf486575d44c4e58a02a72ecc53f183f9
Tassio Neves <tassions@hotmail.com>
2026-09-02 14:50:45 -0300

    feat: enhance Login screen with navigation to Register

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { PublicStackParamsList } from "@/routes";
 
 export const Login = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<PublicStackParamsList>>();
+
   return (
-    <View className="flex-1 items-center justify-center  ">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-blue-800 text-3xl">Tela de login!</Text>
+      <TouchableOpacity
+        className="bg-blue-800 px-4 py-2 rounded-md mt-4"
+        onPress={() => navigation.navigate("Register")}
+      >
+        <Text className="text-white text-lg">Ir para Registro</Text>
+      </TouchableOpacity>
     </View>
   );
 };
