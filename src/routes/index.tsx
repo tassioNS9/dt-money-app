5afa92d65f21b14ed279a744bcb19b3186409abf
Tassio Neves <tassions@hotmail.com>
2026-09-02 15:22:43 -0300

    feat: integrate react-native-edge-to-edge for improved system bar handling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { PublicRoutes } from "./PublicRoutes";
 import { useCallback, useState } from "react";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { SystemBars } from "react-native-edge-to-edge";
 
 const NavigationRoutes = () => {
-  const [user, setUser] = useState({
-    name: "",
-  });
+  const [user, setUser] = useState(undefined);
 
   const Routes = useCallback(() => {
     if (!user) {
@@ -18,6 +17,7 @@ const NavigationRoutes = () => {
 
   return (
     <NavigationContainer>
+      <SystemBars style="light" />
       <Routes />
     </NavigationContainer>
   );
