30daa2ab847e67545d2034f3a3388f83add67122
Tassio Neves <tassions@hotmail.com>
2026-09-02 15:29:11 -0300

    feat: refactor Login screen to use LoginForm and remove unused components

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,23 +1,13 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { PublicStackParamsList } from "@/routes/PublicRoutes";
 import { DismissKeyboardView } from "@/components/DismissKeyboard";
+import { View } from "react-native";
+import { LoginForm } from "./LoginForm";
 
 export const Login = () => {
-  const navigation =
-    useNavigation<StackNavigationProp<PublicStackParamsList>>();
-
   return (
     <DismissKeyboardView>
-      <Text className="text-blue-800 text-3xl">Tela de login!</Text>
-      <TextInput className="bg-gray-500 w-full" />
-      <TouchableOpacity
-        className="bg-blue-800 px-4 py-2 rounded-md mt-4"
-        onPress={() => navigation.navigate("Register")}
-      >
-        <Text className="text-white text-lg">Ir para Registro</Text>
-      </TouchableOpacity>
+      <View className="flex-1 w-[82%] self-center">
+        <LoginForm />
+      </View>
     </DismissKeyboardView>
   );
 };
