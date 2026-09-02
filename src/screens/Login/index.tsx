4455ccaaf1fde200742dc22d627fe08def095236
Tassio Neves <tassions@hotmail.com>
2026-09-02 15:22:49 -0300

    feat: enhance Login screen with DismissKeyboardView and TextInput

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,21 +1,23 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { PublicStackParamsList } from "@/routes/PublicRoutes";
+import { DismissKeyboardView } from "@/components/DismissKeyboard";
 
 export const Login = () => {
   const navigation =
     useNavigation<StackNavigationProp<PublicStackParamsList>>();
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <DismissKeyboardView>
       <Text className="text-blue-800 text-3xl">Tela de login!</Text>
+      <TextInput className="bg-gray-500 w-full" />
       <TouchableOpacity
         className="bg-blue-800 px-4 py-2 rounded-md mt-4"
         onPress={() => navigation.navigate("Register")}
       >
         <Text className="text-white text-lg">Ir para Registro</Text>
       </TouchableOpacity>
-    </View>
+    </DismissKeyboardView>
   );
 };
