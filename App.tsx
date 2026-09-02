b8b14c7487d312e59d95a8eaab9df4a029fa8cab
Tassio Neves <tassions@hotmail.com>
2026-09-01 21:36:55 -0300

    chore: add setup initial project and nativewind

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+import { Login } from "./src/screens/Login";
+import "@/styles/global.css";
+
+export default function App() {
+  return <Login />;
+}
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export const Login = () => {
+  return (
+    <View className="flex-1 items-center justify-center  ">
+      <Text className="text-blue-800 text-3xl">Tela de login!</Text>
+    </View>
+  );
+};
