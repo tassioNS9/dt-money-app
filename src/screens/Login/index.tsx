0404e8250a4915e749a5a43eb0b44865d4847cb3
Tassio Neves <tassions@hotmail.com>
2026-09-02 19:29:42 -0300

    feat: add AuthHeader component to Login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,11 +1,13 @@
 import { DismissKeyboardView } from "@/components/DismissKeyboard";
 import { View } from "react-native";
 import { LoginForm } from "./LoginForm";
+import { AuthHeader } from "@/components/AuthHeader";
 
 export const Login = () => {
   return (
     <DismissKeyboardView>
       <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
         <LoginForm />
       </View>
     </DismissKeyboardView>
