335d25d4cef8510561897847cfb5d0204ac7ce9c
Tassio Neves <tassions@hotmail.com>
2026-09-02 20:16:56 -0300

    feat: add error handling to AppInput component

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -39,7 +39,7 @@ export const AppInput = <T extends FieldValues>({
     <Controller
       control={control}
       name={name}
-      render={({ field: { onChange, value } }) => {
+      render={({ field: { onChange, value }, fieldState: { error } }) => {
         return (
           <View className="w-full mt-4">
             {lable && (
