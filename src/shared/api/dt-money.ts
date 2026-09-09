4992c0cfb4d0815b859dcd20e8bb4e6b70dc2878
Tassio Neves <tassions@hotmail.com>
2026-09-09 20:45:31 -0300

    feat: add response interceptor to handle errors with AppError

--- src/shared/api/dt-money.ts
+++ src/shared/api/dt-money.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Platform } from "react-native";
+import { AppError } from "../helpers/AppError";
 
 const baseURL = Platform.select({
   ios: "http://localhost:3001",
@@ -9,3 +10,14 @@ const baseURL = Platform.select({
 export const dtMoneyApi = axios.create({
   baseURL,
 });
+
+dtMoneyApi.interceptors.response.use(
+  (config) => config,
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(new AppError("Falha na requisição"));
+    }
+  },
+);
