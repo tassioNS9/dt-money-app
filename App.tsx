a4b1179408d716726e4d9d99afefd42bd238d07c
Tassio Neves <tassions@hotmail.com>
2026-09-09 20:59:26 -0300

    feat: wrap AuthContextProvider with SnackbarContextProvider in App component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import "@/styles/global.css";
 import NavigationRoutes from "@/routes";
 import { AuthContextProvider } from "@/context/auth.context";
+import { SnackbarContextProvider } from "@/context/snackbar.context";
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <NavigationRoutes />
-    </AuthContextProvider>
+    <SnackbarContextProvider>
+      <AuthContextProvider>
+        <NavigationRoutes />
+      </AuthContextProvider>
+    </SnackbarContextProvider>
   );
 }
