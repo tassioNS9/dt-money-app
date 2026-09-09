e1ceb05d964237ffd7cdece282c8d652d75fb285
Tassio Neves <tassions@hotmail.com>
2026-09-09 20:45:46 -0300

    feat: add logout button to Home screen with handleLogout functionality

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { useAuthContext } from "@/context/auth.context";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export const Home = () => {
+  const { handleLogout } = useAuthContext();
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 };
