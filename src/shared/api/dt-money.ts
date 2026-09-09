d5b49fe1b176e863628a8498f32b70a529d312ac
Tassio Neves <tassions@hotmail.com>
2026-09-09 15:29:18 -0300

    fix: update Android base URL to use localhost for consistency

--- src/shared/api/dt-money.ts
+++ src/shared/api/dt-money.ts
@@ -3,7 +3,7 @@ import { Platform } from "react-native";
 
 const baseURL = Platform.select({
   ios: "http://localhost:3001",
-  android: "http://10.0.2.2:3001",
+  android: "http://localhost:3001",
 });
 
 export const dtMoneyApi = axios.create({
