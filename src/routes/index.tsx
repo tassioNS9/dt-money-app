c4ae3ac41020abf8b058c814accad657bcfd625d
Tassio Neves <tassions@hotmail.com>
2026-09-09 20:35:45 -0300

    feat: add loading state and integrate Loading component in NavigationRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { PublicRoutes } from "./PublicRoutes";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { PrivateRoutes } from "./PrivateRoutes";
 import { SystemBars } from "react-native-edge-to-edge";
-import { useAuthContext } from "@/Context/auth.context";
+import { useAuthContext } from "@/context/auth.context";
+import { Loading } from "@/screens/Loading";
 
 const NavigationRoutes = () => {
+  const [loading, setLoading] = useState(true);
   const { user, token } = useAuthContext();
 
   const Routes = useCallback(() => {
+    if (loading) {
+      return <Loading setLoading={setLoading} />;
+    }
     if (user && token) {
       return <PrivateRoutes />;
     } else {
       return <PublicRoutes />;
     }
-  }, [user, token]);
+  }, [user, token, loading]);
 
   return (
     <NavigationContainer>
