cc42b482cb018cc59bccb8a0dbca170ab1b90d1e
Tassio Neves <tassions@hotmail.com>
2026-09-10 14:33:50 -0300

    refactor: replace View with SafeAreaView in Home screen for better layout handling

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,17 @@
+import { AppHeader } from "@/components/AppHeader";
 import { useAuthContext } from "@/context/auth.context";
 import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
   const { handleLogout } = useAuthContext();
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <AppHeader />
       <TouchableOpacity onPress={handleLogout}>
         <Text>Sair</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
