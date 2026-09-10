17c30884971fec898e453946e3f51204c90a5ed5
Tassio Neves <tassions@hotmail.com>
2026-09-10 11:05:36 -0300

    feat: add Snackbar component to App for notifications

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import "@/styles/global.css";
 import NavigationRoutes from "@/routes";
 import { AuthContextProvider } from "@/context/auth.context";
 import { SnackbarContextProvider } from "@/context/snackbar.context";
+import { Snackbar } from "@/components/Snackbar";
 
 export default function App() {
   return (
     <SnackbarContextProvider>
       <AuthContextProvider>
         <NavigationRoutes />
+        <Snackbar />
       </AuthContextProvider>
     </SnackbarContextProvider>
   );
