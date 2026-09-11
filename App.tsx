757daeefa98e9dcc334e164c4277a2ad6af9439c
Tassio Neves <tassions@hotmail.com>
2026-09-11 10:15:06 -0300

    feat: add GestureHandlerRootView in app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import "@/styles/global.css";
+
 import NavigationRoutes from "@/routes";
 import { AuthContextProvider } from "@/context/auth.context";
 import { SnackbarContextProvider } from "@/context/snackbar.context";
 import { Snackbar } from "@/components/Snackbar";
 import { BottomSheetProvider } from "@/context/bottomsheet.context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <SnackbarContextProvider>
-      <AuthContextProvider>
-        <BottomSheetProvider>
-          <NavigationRoutes />
-          <Snackbar />
-        </BottomSheetProvider>
-      </AuthContextProvider>
-    </SnackbarContextProvider>
+    <GestureHandlerRootView className="flex-1">
+      <SnackbarContextProvider>
+        <AuthContextProvider>
+          <BottomSheetProvider>
+            <NavigationRoutes />
+            <Snackbar />
+          </BottomSheetProvider>
+        </AuthContextProvider>
+      </SnackbarContextProvider>
+    </GestureHandlerRootView>
   );
 }
