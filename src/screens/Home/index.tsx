d69cefe5fc2bd26cf22547ceeef0723a0453b2ad
Tassio Neves <tassions@hotmail.com>
2026-09-11 13:28:53 -0300

    feat: fetch transaction categories on Home screen load

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,34 @@
 import { AppHeader } from "@/components/AppHeader";
 import { useAuthContext } from "@/context/auth.context";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useTransactionContext } from "@/context/transaction.context";
+import { useErrorHandler } from "@/shared/hooks/useErrorHandler";
+import { useEffect } from "react";
+import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
   const { handleLogout } = useAuthContext();
+  const { fetchCategories } = useTransactionContext();
+  const { handleError } = useErrorHandler();
+
+  const handleFetchCategories = async () => {
+    try {
+      await fetchCategories();
+    } catch (error) {
+      handleError(error);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      await handleFetchCategories();
+    })();
+  }, []);
 
   return (
     <SafeAreaView className="flex-1 bg-background-primary">
       <AppHeader />
+      <Text>HomeScreen</Text>
       <TouchableOpacity onPress={handleLogout}>
         <Text>Sair</Text>
       </TouchableOpacity>
