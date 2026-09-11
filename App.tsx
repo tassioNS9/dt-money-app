13e31be9e8dde2e5460feb2edb002e1a52fe6d1d
Tassio Neves <tassions@hotmail.com>
2026-09-11 09:37:09 -0300

    feat: add BottomSheetProvider in App

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import NavigationRoutes from "@/routes";
 import { AuthContextProvider } from "@/context/auth.context";
 import { SnackbarContextProvider } from "@/context/snackbar.context";
 import { Snackbar } from "@/components/Snackbar";
+import { BottomSheetProvider } from "@/context/bottomsheet.context";
 
 export default function App() {
   return (
     <SnackbarContextProvider>
       <AuthContextProvider>
-        <NavigationRoutes />
-        <Snackbar />
+        <BottomSheetProvider>
+          <NavigationRoutes />
+          <Snackbar />
+        </BottomSheetProvider>
       </AuthContextProvider>
     </SnackbarContextProvider>
   );
