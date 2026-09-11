6e1db1252b07e503dc36cef0b604dd94751c0a72
Tassio Neves <tassions@hotmail.com>
2026-09-11 12:14:43 -0300

    feat: replace inline BottomSheet content with NewTransaction component

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -3,6 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/shared/colors";
 import { useAuthContext } from "@/context/auth.context";
 import { useBottomSheetContext } from "@/context/bottomsheet.context";
+import { NewTransaction } from "../NewTransaction";
 
 export const AppHeader = () => {
   const { handleLogout } = useAuthContext();
@@ -27,12 +28,7 @@ export const AppHeader = () => {
 
       <TouchableOpacity
         onPress={() => {
-          openBottomSheet(
-            <View className="flex-1 items-center justify-center">
-              <Text>Formulário da nova transação</Text>
-            </View>,
-            0,
-          );
+          openBottomSheet(<NewTransaction />, 0);
         }}
         className="bg-accent-brand w-[130px] items-center justify-center rounded-xl h-[50px]"
       >
