5fcb83b63570ac2a1fc72a8f3135a9acdfc983d9
Tassio Neves <tassions@hotmail.com>
2026-09-12 14:42:29 -0300

    style: add background color to AppHeader component

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -10,7 +10,7 @@ export const AppHeader = () => {
   const { openBottomSheet } = useBottomSheetContext();
 
   return (
-    <View className="w-full flex-row p-8 justify-between">
+    <View className="w-full flex-row p-8 justify-between bg-background-primary">
       <View>
         <Image
           source={require("@/assets/Logo.png")}
