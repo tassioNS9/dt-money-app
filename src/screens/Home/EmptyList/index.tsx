df4cef678074455c1533fbfbf20ec25bfea2ce44
Tassio Neves <tassions@hotmail.com>
2026-09-13 20:53:06 -0300

    feat: add EmptyList component

---
+++ src/screens/Home/EmptyList/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export const EmptyList = () => {
+  return (
+    <Text className="text-center text-gray-600 text-lg mt-4">
+      Nenhuma transação encontrada
+    </Text>
+  );
+};
