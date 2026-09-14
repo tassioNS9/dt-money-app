33293f3b076ab43554a14e051efc2157f9f1e98d
Tassio Neves <tassions@hotmail.com>
2026-09-14 15:57:56 -0300

    refactor: enhance TransactionCard to display date information based on filters

--- src/screens/Home/ListHeader/TransactionCard/index.tsx
+++ src/screens/Home/ListHeader/TransactionCard/index.tsx
@@ -1,4 +1,3 @@
-import { colors } from "@/shared/colors";
 import { TransactionTypes } from "@/shared/enums/transaction-types";
 import { FC } from "react";
 import { Text, View } from "react-native";
@@ -22,11 +21,41 @@ export const TransactionCard: FC<Props> = ({ type, amount }) => {
   const iconData = ICONS[type];
   const cardData = CARD_DATA[type];
 
-  const { transactions } = useTransactionContext();
+  const { transactions, filters } = useTransactionContext();
 
   const lastTransaction = transactions.find(
     ({ type: transactionType }) => transactionType.id === type,
   );
+
+  const renderDateInfo = () => {
+    if (type === "total") {
+      return (
+        <Text className="text-white text-base">
+          {filters.from && filters.to
+            ? `${format(filters.from, "d MMMM", { locale: ptBR })} até ${format(
+                filters.to,
+                "d MMMM",
+                { locale: ptBR },
+              )}`
+            : "Todo período"}
+        </Text>
+      );
+    } else {
+      return (
+        <Text className="text-gray-700">
+          {lastTransaction?.createdAt
+            ? format(
+                lastTransaction?.createdAt,
+                `'Última ${cardData.label.toLocaleLowerCase()} em' d 'de' MMMM`,
+                {
+                  locale: ptBR,
+                },
+              )
+            : "Nenhuma transação encontrada"}
+        </Text>
+      );
+    }
+  };
 
   return (
     <View
@@ -43,19 +72,7 @@ export const TransactionCard: FC<Props> = ({ type, amount }) => {
         <Text className="text-2xl text-gray-400 font-bold">
           R$ {moneyMapper(amount)}
         </Text>
-        {type !== "total" && (
-          <Text className="text-gray-700">
-            {lastTransaction?.createdAt
-              ? format(
-                  lastTransaction?.createdAt,
-                  `'Última ${cardData.label.toLocaleLowerCase()} em' d 'de' MMMM`,
-                  {
-                    locale: ptBR,
-                  },
-                )
-              : "Nenhuma transação encontrada"}
-          </Text>
-        )}
+        {renderDateInfo()}
       </View>
     </View>
   );
