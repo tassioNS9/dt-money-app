d87e52fceb04dacaf4ea7a7a315c16efd8d70fb1
Tassio Neves <tassions@hotmail.com>
2026-09-14 21:57:24 -0300

    feat: enhance AppButton component to support custom className and widthFull prop

--- src/components/AppButton/index.tsx
+++ src/components/AppButton/index.tsx
@@ -8,12 +8,15 @@ type AppButtonMode = "fill" | "outline";
 interface AppButtonParams extends TouchableOpacityProps {
   mode?: AppButtonMode;
   iconName?: keyof typeof MaterialIcons.glyphMap;
+  widthFull?: boolean;
 }
 
 export const AppButton: FC<PropsWithChildren<AppButtonParams>> = ({
   children,
   mode = "fill",
   iconName,
+  className,
+  widthFull = true,
   ...rest
 }) => {
   const isFill = mode === "fill";
@@ -22,7 +25,9 @@ export const AppButton: FC<PropsWithChildren<AppButtonParams>> = ({
     <TouchableOpacity
       {...rest}
       className={clsx(
-        "w-full rounded-xl px-5 flex-row items-center h-button",
+        widthFull && "w-full",
+        className,
+        "rounded-xl px-5 flex-row items-center h-button",
         iconName ? "justify-between" : "justify-center",
         {
           "bg-accent-brand": isFill,
